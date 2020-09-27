57d9b7fb3e737db8eaa321dba517c96794aecd37
Benjamin Garcia <48642473+benjamingarcia10@users.noreply.github.com>
2020-09-27 02:48:30 -0700

    Updated token features

--- auth.py
+++ auth.py
@@ -27,10 +27,10 @@ class AuthorizedUser(db.Model):
                 'ip_address': self.ip_address, 'admin': self.admin}
 
 
-def add_admin_account(name, password):
+def add_admin_account(name, password, ip_address):
     hashed_password = generate_password_hash(password, method='sha256')
 
-    new_user = AuthorizedUser(public_id=str(uuid.uuid4()), ip_address='', name=name,
+    new_user = AuthorizedUser(public_id=str(uuid.uuid4()), ip_address=ip_address, name=name,
                               password=hashed_password, admin=True)
     db.session.add(new_user)
     db.session.commit()
@@ -45,7 +45,7 @@ def token_required(func):
             token = request.headers['x-access-token']
 
         if not token:
-            return jsonify({'message': 'Token is missing!'}), 401
+            return jsonify({'message': 'Token is missing from x-access-token header!'}), 401
 
         try:
             data = jwt.decode(token, app.config['SECRET_KEY'])
@@ -94,16 +94,18 @@ def create_user(current_user):
     if not current_user.admin:
         return jsonify({'message': 'Cannot perform that function!'})
 
-    data = request.get_json()
+    try:
+        data = request.get_json()
+        hashed_password = generate_password_hash(data['password'], method='sha256')
 
-    hashed_password = generate_password_hash(data['password'], method='sha256')
+        new_user = AuthorizedUser(public_id=str(uuid.uuid4()), ip_address=str(request.remote_addr), name=data['name'],
+                                  password=hashed_password, admin=False)
+        db.session.add(new_user)
+        db.session.commit()
 
-    new_user = AuthorizedUser(public_id=str(uuid.uuid4()), ip_address=str(request.remote_addr), name=data['name'],
-                              password=hashed_password, admin=False)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({'message': 'New user created!'})
+        return jsonify({'message': 'New user created!'})
+    except:
+        return jsonify({'message': 'Could not create new user. Verify that required data is passed.'})
 
 
 @app.route('/user/<public_id>', methods=['PUT'])
@@ -152,16 +154,19 @@ def login():
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
 
     if check_password_hash(user.password, auth.password):
+        if str(request.remote_addr) != user.ip_address:
+            return make_response('Unauthorized IP Address', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
         token = jwt.encode(
             {'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
             app.config['SECRET_KEY'])
-
-        return jsonify({'token': token.decode('UTF-8')})
+        data = user.get_user_data()
+        data['token'] = token.decode('UTF-8')
+        return data
 
     return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
 
 
 if __name__ == '__main__':
     # db.create_all()
-    # add_admin_account('Admin', 'Admin100!')
+    # add_admin_account('Admin', 'Admin100!', '127.0.0.1')
     app.run(debug=True)
